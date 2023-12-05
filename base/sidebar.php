cd9c7ef62a7e437e4fdef95056c9097ce737f76a
dghant <152829011+giahan3@users.noreply.github.com>
2023-12-05 07:47:07 +0700

    Add files via upload

---
+++ base/sidebar.php
@@ -0,0 +1,53 @@
+<div class="side-bar">
+    <ul class="sidebar-item-wraper">
+        <li class="sidebar-item">
+            <a href="../pages/main.php">
+                <div class="sidebar-item-icon">
+                    <i class="bi bi-house"></i>
+                </div>
+                <div class="sidebar-item-text">Dashboard</div>
+            </a>
+        </li>
+        <li class="sidebar-item">
+            <a href="../pages/employee.php">
+                <div class="sidebar-item-icon">
+                    <i class="bi bi-people-fill"></i>
+                </div>
+                <div class="sidebar-item-text">Employee</div>
+            </a>
+        </li>
+        <li class="sidebar-item">
+            <a href="../pages/suppliers.php">
+                <div class="sidebar-item-icon">
+                <i class="bi bi-building-fill-gear"></i>
+                </div>
+                <div class="sidebar-item-text">Suppliers</div>
+            </a>
+        </li>
+        
+        <li class="sidebar-item">
+            <a href="../pages/categories.php">
+                <div class="sidebar-item-icon">
+                    <i class="bi bi-tag"></i>
+                </div>
+                <div class="sidebar-item-text">Categories</div>
+            </a>
+        </li>
+        <li class="sidebar-item">
+            <a href="../pages/customer.php">
+                <div class="sidebar-item-icon">
+                    <i class="bi bi-people"></i>
+                </div>
+                <div class="sidebar-item-text">Customer</div>
+            </a>
+        </li>
+        <li class="sidebar-item">
+            <a href="../pages/order.php">
+                <div class="sidebar-item-icon">
+                    <i class="bi bi-receipt"></i>
+                </div>
+                <div class="sidebar-item-text">Order</div>
+            </a>
+        </li>
+    </ul>
+</div>
