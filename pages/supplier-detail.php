ad4745c804738113bf5eee7e5de4933d984b7854
dghant <152829011+giahan3@users.noreply.github.com>
2023-12-05 09:39:40 +0700

    Update supplier-detail.php

--- pages/supplier-detail.php
+++ pages/supplier-detail.php
@@ -32,21 +32,14 @@
                 <p class="address"><b>Supplier NO: </b><?php echo $row['sup_code']?></p>
                     <div class="shipping-info-head">
                         <h6>Name: </h6>
-                        
                         <p><?php echo $row['name']?> </p>
-          
                         <h6>Address: </h6>
-
                         <p><?php echo $row['address']?></p>
-                        
                     </div>
                     <div class="shipping-info">
                         <h6>Bank Account: </h6>
-                        
                         <p><?php echo $row['bankacount']?> </p>
-          
                         <h6>Tax Code: </h6>
-
                         <p><?php echo $row['taxcode']?></p>
                     </div>
                     <?php } ?>
@@ -56,11 +49,11 @@
                         <h6>COLOR</h6>
                         <h6>QUANTITY</h6>
                         <h6>DATE</h6>
+                        <h6>CURRENT PRICE</h6>
                     </div>
                 <hr class="bottom">
                 <?php $id=$_GET['id'];
-                $sqlcus ="SELECT   `category`.`name`,`category`.`color`,`category`.`quantity`,`bolt`.`date` 
-                 FROM `category` INNER JOIN `supplier`INNER JOIN `bolt` WHERE `supplier`.`sup_code`=`category`.`sup_code` AND `category`.`cate_code`=`bolt`.`cate_code` AND`supplier`.`sup_code`=$id;";
+                $sqlcus ="SELECT `category`.`name`,`category`.`color`,`category`.`quantity`,`bolt`.`date`, `currentprice`.`cate_price` FROM `category` INNER JOIN `supplier` INNER JOIN `currentprice` INNER JOIN `bolt` WHERE `supplier`.`sup_code`=`category`.`sup_code` AND `category`.`cate_code`=`bolt`.`cate_code` AND `category`.`cate_code`=`currentprice`.`cate_code` AND`supplier`.`sup_code`=$id;";
                 $querybolt = mysqli_query($conn, $sqlcus);
                 while($row = mysqli_fetch_array($querybolt)){
                     ?>
@@ -69,7 +62,9 @@
                     <p><?php echo $row['color']?></p>
                     <p><?php echo $row['quantity']?></p>
                     <p><?php echo $row['date']?></p>
-                </div><?php } ?>
+                    <p><?php echo $row['cate_price']?></p>
+                </div>
+                <?php } ?>
             </div>
         </div><br>
         <br>
